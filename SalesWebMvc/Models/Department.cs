80d0e6da18eea32d1a3e11e03d9c0cc6baaf4fdf
Ronaldo Borges Guimarães <ronaldobgui@outlook.com>
2022-06-27 14:36:17 -0400

    initial commit

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+  public class Department
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public ICollection<Seller> Sellers { get; set; }
+    public Department()
+    {
+      Sellers = new List<Seller>();
+    }
+    public Department(int id, string name) : this()
+    {
+      Id = id;
+      Name = name;
+    }
+    public void AddSeller(Seller seller)
+    {
+      Sellers.Add(seller);
+    }
+    public double TotalSales(DateTime initial, DateTime final)
+    {
+      return Sellers.Sum(s => s.TotalSales(initial, final));
+    }
+  }
+}
